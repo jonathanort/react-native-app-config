6183e84873f61d9065875deaed4b1d65423eadd3
Jonathan Ortheden <jonathan@appuniver.se>
2019-08-05 15:03:57 +0200

    Add android package

--- android/src/main/java/com/reactlibrary/RNAppConfigModule.java
+++ android/src/main/java/com/reactlibrary/RNAppConfigModule.java
@@ -1,10 +1,19 @@
 
 package com.reactlibrary;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.provider.Settings;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class RNAppConfigModule extends ReactContextBaseJavaModule {
 
@@ -19,4 +28,36 @@ public class RNAppConfigModule extends ReactContextBaseJavaModule {
   public String getName() {
     return "RNAppConfig";
   }
+
+  private String getBuildType() {
+    switch (BuildConfig.BUILD_TYPE.toLowerCase()) {
+        case "debug": return "dev";
+        case "release": return "release";
+    }
+
+    return "release";
+  }
+
+   @Override
+    public @Nullable
+    Map<String, Object> getConstants() {
+        HashMap<String, Object> constants = new HashMap<String, Object>();
+
+        PackageManager packageManager = this.reactContext.getPackageManager();
+        String packageName = this.reactContext.getPackageName();
+
+        constants.put("appVersion", "unknown");
+
+        try {
+            PackageInfo info = packageManager.getPackageInfo(packageName, 0);
+            if (info.versionName != null && !info.versionName.isEmpty())
+                constants.put("appVersion", info.versionName);
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+
+        constants.put("uniqueId", Settings.Secure.getString(this.reactContext.getContentResolver(), Settings.Secure.ANDROID_ID));
+        constants.put("buildType",getBuildType());
+
+        return constants;
+    }
 }
